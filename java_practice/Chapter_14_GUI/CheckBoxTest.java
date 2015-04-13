1668b8c6983277d19b486ba9e14339973641ebfc
Hoon Kim <kimx2815@umn.edu>
2015-04-13 18:45:19 -0500

    example of using checkbox

---
+++ java_practice/Chapter_14_GUI/CheckBoxTest.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class CheckBoxTest {
+
+    public static void main(String[] args)  {
+
+	CheckBoxFrame checkBoxFrame = new CheckBoxFrame();
+	checkBoxFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	checkBoxFrame.setSize(500,500);
+	checkBoxFrame.setVisible(true);
+    }
+}
