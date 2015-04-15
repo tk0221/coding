8cedea423d772eacd44de6836efa9109b71f6939
Hoon Kim <kimx2815@umn.edu>
2015-04-14 19:15:35 -0500

    MouseTracking GUI

---
+++ java_practice/Chapter_14_GUI/MouseTracker.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class MouseTracker {
+    public static void main( String[] args ) {
+
+	MouseTrackerFrame m = new MouseTrackerFrame();
+	m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	m.setSize(500, 500);
+	m.setVisible(true);
+    }
+}
+    
