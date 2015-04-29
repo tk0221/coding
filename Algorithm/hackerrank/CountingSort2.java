96f45cad7584febe3622e5e18f0b5d8ca8eafb1e
Taehun Kim <kimx2815@umn.edu>
2015-04-28 19:10:08 -0500

    Create CountingSort2.java

---
+++ Algorithm/hackerrank/CountingSort2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+            
+        int size = in.nextInt();
+        int[] a = new int[size];
+        int[] b = new int[100];//0~99
+        
+        for(int i = 0; i < size; i++){
+            a[i] = in.nextInt();
+            b[a[i]]++;
+            
+        }
+   
+        for(int i = 0; i < size; i++){
+            while(b[i] != 0) {
+                System.out.printf("%d ",i);
+                b[i]--;
+            }
+        }
+    
+    
+    }
+}
